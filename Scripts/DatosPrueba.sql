b11c04aa95e8b4dc14b5df116a596d8dd667cc3d
bydanimos <52750032+bydanimos@users.noreply.github.com>
2020-04-13 11:11:07 +0200

    Scripts creación base de datos

---
+++ Scripts/DatosPrueba.sql
@@ -0,0 +1,2 @@
+insert into usuario (dni, id, correoelectronico, contrasenha, nombre, primerapellido, segundoapellido, paisprocedencia, telefono, sexo) 
+values('35601335A', 'dani', 'dani@gmail.com', 'root', 'dani', 'mos', 'ang', 'Españita', '686454435', 'h')
