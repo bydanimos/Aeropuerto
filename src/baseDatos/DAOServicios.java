15f63456e3d19000080ee518b06234074f655e2f
danimosteiro25@gmail.com <danimosteiro25@gmail.com>
2020-04-21 17:04:23 +0200

    Tiendas - Terminales

--- src/baseDatos/DAOServicios.java
+++ src/baseDatos/DAOServicios.java
@@ -4,9 +4,11 @@
  */
 
 package baseDatos;
-import aplicacion.*;
+
+import aplicacion.aviones.Terminal;
 import aplicacion.servicios.Tienda;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -18,6 +20,86 @@ public class DAOServicios extends AbstractDAO {
     }
 
     public List<Tienda> obtenerTiendas(String nombre, int terminal) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Tienda> resultado = new ArrayList<>();
+        ArrayList<Terminal> terminales = this.obtenerTerminales();
+        Tienda tiendaActual;
+        Terminal terminalAux;
+        Connection con;
+        PreparedStatement stmTiendas = null;
+        ResultSet rsTienda;
+
+        con = this.getConexion();
+
+        String consulta = "select terminal, codigo, tipoventas, nombre "
+                + "from tienda "
+                + "where nombre like ? "
+                + "and terminal = ?";
+
+        try {
+            stmTiendas = con.prepareStatement(consulta);
+            stmTiendas.setString(1, "%" + nombre + "%");
+            stmTiendas.setInt(2, terminal);
+
+            rsTienda = stmTiendas.executeQuery();
+            while (rsTienda.next()) {
+                terminalAux = terminales.get(rsTienda.getInt("terminal") - 1);
+                tiendaActual = new Tienda(terminalAux, 
+                        rsTienda.getInt("codigo"), 
+                        rsTienda.getString("tipoventas"),
+                        rsTienda.getString("nombre"));
+
+                resultado.add(tiendaActual);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            this.getFachadaAplicacion().muestraExcepcion(e.getMessage());
+        } finally {
+            try {
+                stmTiendas.close();
+            } catch (SQLException e) {
+                System.out.println("Imposible cerrar cursores");
+            }
+        }
+        return resultado;
+    }
+    
+    public ArrayList<Terminal> obtenerTerminales() {
+        ArrayList<Terminal> resultado = new ArrayList<>();
+        Terminal terminalActual;
+        Connection con;
+        PreparedStatement stmTerminal = null;
+        ResultSet rsTerminal;
+
+        con = this.getConexion();
+
+        String consulta = "select numero, primeraPuerta, ultimaPuerta "
+                + "from terminal "
+                + "order by numero";
+
+        try {
+            stmTerminal = con.prepareStatement(consulta);
+
+            rsTerminal = stmTerminal.executeQuery();
+            while (rsTerminal.next()) {
+
+                terminalActual = new Terminal(rsTerminal.getInt("numero"), 
+                        rsTerminal.getInt("primeraPuerta"), 
+                        rsTerminal.getInt("ultimaPuerta"));
+
+                resultado.add(terminalActual);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            this.getFachadaAplicacion().muestraExcepcion(e.getMessage());
+        } finally {
+            try {
+                stmTerminal.close();
+            } catch (SQLException e) {
+                System.out.println("Imposible cerrar cursores");
+            }
+        }
+        return resultado;
     }
 }
