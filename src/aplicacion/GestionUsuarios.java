48e6177c40a8e6b123d3d83972242afade791373
bydanimos <52750032+bydanimos@users.noreply.github.com>
2020-04-27 01:12:53 +0200

    Update GestionUsuarios.java

--- src/aplicacion/GestionUsuarios.java
+++ src/aplicacion/GestionUsuarios.java
@@ -9,11 +9,15 @@ public class GestionUsuarios {
     private final FachadaGui fgui;
     private final FachadaBaseDatos fbd;
 
+    // ------------------------------------------------------------------------
+    // ----------------------------- Constructor ------------------------------
     public GestionUsuarios(FachadaGui fgui, FachadaBaseDatos fbd) {
         this.fgui = fgui;
         this.fbd = fbd;
     }
 
+    // ------------------------------------------------------------------------
+    // ------------------------------- Acceder --------------------------------
     public boolean comprobarAutentificacion(String idUsuario, String clave) {
         Usuario u;
 
@@ -36,32 +40,36 @@ public class GestionUsuarios {
         }
     }
     
+    // ------------------------------------------------------------------------
+    // ------------------------------ Registrar -------------------------------
     public void nuevoUsuario() {
         this.fgui.nuevoUsuario();
-    }
-    
-    public boolean registrarUsuario(Usuario usuario) {
-        return this.fbd.registrarUsuario(usuario);
-    }
-    
-    public java.util.List<Usuario> obtenerUsuarios(String dni, String id, String nombre, String primerApellido, String segundoApellido){
-        return this.fbd.consultarRegistroUsuarios(id, dni, nombre, primerApellido, segundoApellido);
     }
 
     public boolean comprobarId(String text) {
         return this.fbd.comprobarId(text);
+    }
+    
+    public boolean registrarUsuario(Usuario usuario){
+        return this.fbd.registrarUsuario(usuario);
+    }
+    
+    // ------------------------------------------------------------------------
+    // ------------------------------ Usuarios --------------------------------
+    public java.util.List<Usuario> obtenerUsuarios(String dni, String id, String nombre, String primerApellido, String segundoApellido){
+        return this.fbd.consultarRegistroUsuarios(id,dni,nombre,primerApellido,segundoApellido);
     }
 
     public String getTipoUsuario(Usuario u){
         return fbd.getTipoUsuario(u);
     }
     
-    public void modificarUsuario(Usuario u, String tipoAnterior, String tipoNuevo) {
-        this.fbd.modificarUsuarioGeneral(u, tipoAnterior, tipoNuevo);
+    public void modificarUsuario(Usuario u, String tipoAnterior, String tipoNuevo){
+        this.fbd.modificarUsuarioGeneral(u,tipoAnterior,tipoNuevo);
     }
     
     public java.util.List<Usuario> obtenerUsuariosControl(String dni, String id, String nombre, String primerApellido, String segundoApellido){
-        return this.fbd.obtenerUsuariosControl(dni, id, nombre, primerApellido, segundoApellido);
+        return this.fbd.obtenerUsuariosControl(dni,id,nombre,primerApellido,segundoApellido);
     }
     
     public boolean borrarUsuario(Usuario u){
