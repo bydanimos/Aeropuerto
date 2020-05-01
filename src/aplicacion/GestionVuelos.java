740c617f9ad4446005919e4cdb778f7109f3790b
rubeeenom <56005357+rubeeenom@users.noreply.github.com>
2020-05-01 13:21:06 +0200

    Estadísticas vuelos

--- src/aplicacion/GestionVuelos.java
+++ src/aplicacion/GestionVuelos.java
@@ -26,4 +26,24 @@ public class GestionVuelos {
     public boolean guardarVuelo(Vuelo vuelo) {
         return this.fbd.guardarVuelo(vuelo);
     }
+    
+    //---------------------------------Estadísticas Vuelos------------------
+    public Aerolinea getAerolineaVuelo(Vuelo vuelo){
+        return this.fbd.getAerolineaVuelo(vuelo);
+    }
+    
+    public String getNacionalidadMayoritariaVuelo(Vuelo vuelo){
+        return this.fbd.getNacionalidadMayoritariaVuelo(vuelo);
+    }
+    public Integer getNTipoVuelo(Vuelo vuelo,TipoAsiento ta){
+        return this.fbd.getNTipoVuelo(vuelo,ta);
+    }
+    public Integer getNSexoVuelo(Vuelo vuelo, TipoSexo ts){
+        return this.fbd.getNSexoVuelo(vuelo,ts);
+    }
+    
+    public List<Vuelo> obtenerVuelos(String origen,String destino,Timestamp tSalida){
+        return this.fbd.obtenerVuelos(origen, destino,tSalida);
+    }
+    //----------------------------------------------------------------------
 }
