84ca7f7c683930df8196b37f7fa44d6ecca72bf7
bydanimos <52750032+bydanimos@users.noreply.github.com>
2020-05-01 17:17:48 +0200

    Personal Laboral

--- src/aplicacion/usuarios/PersonalLaboral.java
+++ src/aplicacion/usuarios/PersonalLaboral.java
@@ -3,69 +3,104 @@ package aplicacion.usuarios;
 import java.util.ArrayList;
 import java.sql.Timestamp;
 
-public class PersonalLaboral extends Usuario{
+public class PersonalLaboral extends Usuario {
 
     private String labor;
     private String descripcionTarea;
     private ArrayList<Horario> historialTrabajo;
     private Timestamp fechaInicio;
+    private boolean deServicio;
+    private Timestamp fechaEntrada;
 
-    public PersonalLaboral(String dni, String id, String contrasenha, String correoElectronico, String nombre,
-                           String apellido1, String apellido2, TipoSexo sexo, String paisProcedencia, Integer telefono,
-                           String labor, String descripcionTarea, ArrayList<Horario> historialTrabajo, Timestamp fechaInicio){
-        super(dni, id, contrasenha, correoElectronico, nombre, apellido1, apellido2, sexo, paisProcedencia, telefono);
+    // -------------------------------------------------------------------------
+    // ---------------------------- Constructores ------------------------------
+    public PersonalLaboral(String dni, String id, String contrasenha, 
+            String correoElectronico, String nombre, String apellido1, 
+            String apellido2, TipoSexo sexo, String paisProcedencia, 
+            Integer telefono, String labor, String descripcionTarea, 
+            ArrayList<Horario> historialTrabajo, Timestamp fechaInicio) {
+        
+        super(dni, id, contrasenha, correoElectronico, nombre, apellido1, 
+                apellido2, sexo, paisProcedencia, telefono);
         this.labor = labor;
         this.descripcionTarea = descripcionTarea;
         this.historialTrabajo = historialTrabajo;
         this.fechaInicio = fechaInicio;
     }
     
-    public PersonalLaboral(String dni, String id, String contrasenha, String correoElectronico, String nombre,
-                           String apellido1, String apellido2, TipoSexo sexo, String paisProcedencia, Integer telefono,
-                           String labor, String descripcionTarea, Timestamp fechaInicio){
-        super(dni, id, contrasenha, correoElectronico, nombre, apellido1, apellido2, sexo, paisProcedencia, telefono);
+    public PersonalLaboral(String dni, String id, String contrasenha, 
+            String correoElectronico, String nombre, String apellido1, 
+            String apellido2, TipoSexo sexo, String paisProcedencia, 
+            Integer telefono, String labor, String descripcionTarea, 
+            Timestamp fechaInicio) {
+        
+        super(dni, id, contrasenha, correoElectronico, nombre, apellido1, 
+                apellido2, sexo, paisProcedencia, telefono);
         this.labor = labor;
         this.descripcionTarea = descripcionTarea;
         this.fechaInicio = fechaInicio;
     }
     
-    public PersonalLaboral(String dni, String id, String contrasenha, String correoElectronico, String nombre,
-                        String apellido1, String apellido2, TipoSexo sexo, String paisProcedencia, Integer telefono){
-        super(dni, id, contrasenha, correoElectronico, nombre, apellido1, apellido2, sexo, paisProcedencia, telefono);
+    public PersonalLaboral(String dni, String id, String contrasenha, 
+            String email, String nombre, String apellido1, String apellido2, 
+            TipoSexo sexo, String paisProcedencia, Integer telefono) {
+        
+        super(dni, id, contrasenha, email, nombre, apellido1, apellido2, 
+                sexo, paisProcedencia, telefono);
         this.labor = "";
         this.descripcionTarea = "";
         this.fechaInicio = null;
     }
 
-    public String getLabor() {
-        return labor;
-    }
-
+    // -------------------------------------------------------------------------
+    // ------------------------------- Setters ---------------------------------
     public void setLabor(String labor) {
         this.labor = labor;
-    }
-
-    public String getDescripcionTarea() {
-        return descripcionTarea;
     }
 
     public void setDescripcionTarea(String descripcionTarea) {
         this.descripcionTarea = descripcionTarea;
     }
 
-    public ArrayList<Horario> getHistorialTrabajo() {
-        return historialTrabajo;
-    }
-
     public void setHistorialTrabajo(ArrayList<Horario> historialTrabajo) {
         this.historialTrabajo = historialTrabajo;
     }
 
-    public Timestamp getFechaInicio() {
-        return fechaInicio;
-    }
-
     public void setFechaInicio(Timestamp fechaInicio) {
         this.fechaInicio = fechaInicio;
+    }
+    
+    public void setDeServicio(boolean deServicio) {
+        this.deServicio = deServicio;
+    }
+    
+    public void setFechaEntrada(Timestamp fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    // -------------------------------------------------------------------------
+    // ------------------------------- Getters ---------------------------------
+    public String getLabor() {
+        return this.labor;
+    }
+
+    public String getDescripcionTarea() {
+        return this.descripcionTarea;
+    }
+
+    public ArrayList<Horario> getHistorialTrabajo() {
+        return this.historialTrabajo;
+    }
+
+    public Timestamp getFechaInicio() {
+        return this.fechaInicio;
+    }
+    
+    public boolean isDeServicio() {
+        return this.deServicio;
+    }
+    
+    public Timestamp getFechaEntrada() {
+        return this.fechaEntrada;
     }
 }
