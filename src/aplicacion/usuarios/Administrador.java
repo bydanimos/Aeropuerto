12039433f1f6d8eeaafd8a218b0d6c4f88c30ed2
Dani <52750032+bydanimos@users.noreply.github.com>
2020-05-10 11:49:40 +0200

    Bonito

--- src/aplicacion/usuarios/Administrador.java
+++ src/aplicacion/usuarios/Administrador.java
@@ -1,37 +1,44 @@
+
 package aplicacion.usuarios;
 
 import java.sql.Timestamp;
 
-public class Administrador extends Usuario{
+public final class Administrador extends Usuario {
 
     private java.sql.Timestamp fechaInicio;
     private String curriculum;
 
+    // -------------------------------------------------------------------------
+    // ------------------------------- Constructor -----------------------------
     public Administrador(String dni, String id, String contrasenha, String correoElectronico, String nombre,
                         String apellido1, String apellido2, TipoSexo sexo, String paisProcedencia, Integer telefono,
-                         java.sql.Timestamp fechaInicio, String curriculum){
+                        java.sql.Timestamp fechaInicio, String curriculum) {
         super(dni, id, contrasenha, correoElectronico, nombre, apellido1, apellido2, sexo, paisProcedencia, telefono);
         this.fechaInicio = fechaInicio;
         this.curriculum = curriculum;
     }
 
     public Administrador(String dni, String id, String contrasenha, String correoElectronico, String nombre,
-                        String apellido1, String apellido2, TipoSexo sexo, String paisProcedencia, Integer telefono){
+                        String apellido1, String apellido2, TipoSexo sexo, String paisProcedencia, Integer telefono) {
         super(dni, id, contrasenha, correoElectronico, nombre, apellido1, apellido2, sexo, paisProcedencia, telefono);
         this.fechaInicio = null;
         this.curriculum = "";
     }
 
+    // -------------------------------------------------------------------------
+    // --------------------------------- Getters -------------------------------
     public Timestamp getFechaInicio() {
         return fechaInicio;
     }
-
-    public void setFechaInicio(Timestamp fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-
+    
     public String getCurriculum() {
         return curriculum;
+    }
+    
+    // -------------------------------------------------------------------------
+    // --------------------------------- Setters -------------------------------
+    public void setFechaInicio(Timestamp fechaInicio) {
+        this.fechaInicio = fechaInicio;
     }
 
     public void setCurriculum(String curriculum) {
