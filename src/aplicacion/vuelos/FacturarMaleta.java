33d76abcb2a54939d61e64751cbb632f11318ae6
Dani <52750032+bydanimos@users.noreply.github.com>
2020-05-10 11:39:33 +0200

    Bonito

--- src/aplicacion/vuelos/FacturarMaleta.java
+++ src/aplicacion/vuelos/FacturarMaleta.java
@@ -8,41 +8,48 @@ public class FacturarMaleta {
     private Maleta maleta;
     private Timestamp fechaFacturacion;
 
+    // -------------------------------------------------------------------------
+    // ------------------------------- Constructor -----------------------------
     public FacturarMaleta(Billete billete, Maleta maleta, Timestamp fechaFacturacion) {
         this.billete = billete;
         this.maleta = maleta;
         this.fechaFacturacion = fechaFacturacion;
     }
 
+    // -------------------------------------------------------------------------
+    // --------------------------------- Getters -------------------------------
     public Billete getBillete() {
         return billete;
     }
-
-    public void setBillete(Billete billete) {
-        this.billete = billete;
-    }
-
+    
     public Maleta getMaleta() {
         return maleta;
+    }
+
+    public Timestamp getFechaFacturacion() {
+        return fechaFacturacion;
+    }
+    
+    public float getPrecioExtra() {
+        if (this.maleta.getPeso() > this.billete.getVuelo().getAvion().getAerolinea().getPesoBaseMaleta()) {
+            float diferencia = this.billete.getVuelo().getAvion().getAerolinea().getPesoBaseMaleta() - 
+                               this.maleta.getPeso();
+            return diferencia * 2;
+        } else
+            return 0;
+    }
+    
+    // -------------------------------------------------------------------------
+    // --------------------------------- Setters -------------------------------
+    public void setBillete(Billete billete) {
+        this.billete = billete;
     }
 
     public void setMaleta(Maleta maleta) {
         this.maleta = maleta;
     }
 
-    public Timestamp getFechaFacturacion() {
-        return fechaFacturacion;
-    }
-
     public void setFechaFacturacion(Timestamp fechaFacturacion) {
         this.fechaFacturacion = fechaFacturacion;
-    }
-
-    public float getPrecioExtra(){
-        if (this.maleta.getPeso() > this.billete.getVuelo().getAvion().getAerolinea().getPesoBaseMaleta()){
-            float diferencia = this.billete.getVuelo().getAvion().getAerolinea().getPesoBaseMaleta() - this.maleta.getPeso();
-            return diferencia * 2;
-        } else
-            return 0;
     }
 }
