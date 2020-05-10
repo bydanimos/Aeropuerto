72c6f1dc93fd77a04918241d7532c313285a8ca1
Dani <52750032+bydanimos@users.noreply.github.com>
2020-05-10 11:50:19 +0200

    Bonito

--- src/aplicacion/usuarios/Horario.java
+++ src/aplicacion/usuarios/Horario.java
@@ -7,6 +7,8 @@ public class Horario {
     private Timestamp fechaEntrada;
     private Timestamp fechaSalida;
 
+    // -------------------------------------------------------------------------
+    // ------------------------------- Constructor -----------------------------
     public Horario(Timestamp fechaEntrada, Timestamp fechaSalida) {
         this.fechaEntrada = fechaEntrada;
         this.fechaSalida = fechaSalida;
