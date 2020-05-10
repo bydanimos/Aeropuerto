53a4ce179abb8dcf6db5a0d2c542bf202619bde3
Dani <52750032+bydanimos@users.noreply.github.com>
2020-05-10 11:22:44 +0200

    Bonito

--- src/baseDatos/AbstractDAO.java
+++ src/baseDatos/AbstractDAO.java
@@ -1,3 +1,4 @@
+
 package baseDatos;
 
 public abstract class AbstractDAO {
@@ -5,19 +6,23 @@ public abstract class AbstractDAO {
     private aplicacion.FachadaAplicacion fa;
     private java.sql.Connection conexion;
 
+    // -------------------------------------------------------------------------
+    // --------------------------------- Getters -------------------------------
     protected java.sql.Connection getConexion() {
         return this.conexion;
     }
 
-    protected void setConexion(java.sql.Connection conexion) {
-        this.conexion = conexion;
+    protected aplicacion.FachadaAplicacion getFachadaAplicacion() {
+        return fa;
     }
 
+    // -------------------------------------------------------------------------
+    // --------------------------------- Setters -------------------------------
     protected void setFachadaAplicacion(aplicacion.FachadaAplicacion fa) {
         this.fa = fa;
     }
 
-    protected aplicacion.FachadaAplicacion getFachadaAplicacion() {
-        return fa;
+    protected void setConexion(java.sql.Connection conexion) {
+        this.conexion = conexion;
     }
 }
