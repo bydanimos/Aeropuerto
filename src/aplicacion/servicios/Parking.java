f3af8f548057794c626a168e208b3660ef0bf56c
Dani <52750032+bydanimos@users.noreply.github.com>
2020-05-10 12:12:47 +0200

    Bonito

--- src/aplicacion/servicios/Parking.java
+++ src/aplicacion/servicios/Parking.java
@@ -1,37 +1,44 @@
+
 package aplicacion.servicios;
 
 import aplicacion.aviones.Terminal;
 
-public class Parking {
+public final class Parking {
 
     private Terminal terminal;
     private int piso;
     private int numeroPlazas;
 
+    // ------------------------------------------------------------------------
+    // ----------------------------- Constructor ------------------------------
     public Parking(Terminal terminal, int piso, int numeroPlazas) {
         this.terminal = terminal;
         this.piso = piso;
         this.numeroPlazas = numeroPlazas;
     }
 
+    // ------------------------------------------------------------------------
+    // ------------------------------- Getters --------------------------------
     public Terminal getTerminal() {
         return terminal;
-    }
-
-    public void setTerminal(Terminal terminal) {
-        this.terminal = terminal;
     }
 
     public int getPiso() {
         return piso;
     }
 
-    public void setPiso(int piso) {
-        this.piso = piso;
-    }
-
     public int getNumeroPlazas() {
         return numeroPlazas;
+    }
+
+    // ------------------------------------------------------------------------
+    // ------------------------------- Setters --------------------------------
+    public void setTerminal(Terminal terminal) {
+        this.terminal = terminal;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
     }
 
     public void setNumeroPlazas(int numeroPlazas) {
