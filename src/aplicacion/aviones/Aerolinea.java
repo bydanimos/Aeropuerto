68e7165b89a44eb3b587e66924e1ea3a00037407
Dani <52750032+bydanimos@users.noreply.github.com>
2020-05-10 12:23:22 +0200

    Bonito

--- src/aplicacion/aviones/Aerolinea.java
+++ src/aplicacion/aviones/Aerolinea.java
@@ -1,12 +1,15 @@
+
 package aplicacion.aviones;
 
-public class Aerolinea {
+public final class Aerolinea {
 
     private String nombre;
     private String paisSede;
     private float precioBaseMaleta;
     private float pesoBaseMaleta;
 
+    // ------------------------------------------------------------------------
+    // ----------------------------- Constructor ------------------------------
     public Aerolinea(String nombre, String paisSede, float precioBaseMaleta, float pesoBaseMaleta) {
         this.nombre = nombre;
         this.paisSede = paisSede;
@@ -14,32 +17,36 @@ public class Aerolinea {
         this.pesoBaseMaleta = pesoBaseMaleta;
     }
 
+    // ------------------------------------------------------------------------
+    // ------------------------------- Getters --------------------------------
     public String getNombre() {
         return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public String getPaisSede() {
         return paisSede;
     }
 
-    public void setPaisSede(String paisSede) {
-        this.paisSede = paisSede;
-    }
-
     public float getPrecioBaseMaleta() {
         return precioBaseMaleta;
     }
 
-    public void setPrecioBaseMaleta(float precioBaseMaleta) {
-        this.precioBaseMaleta = precioBaseMaleta;
-    }
-
     public float getPesoBaseMaleta() {
         return pesoBaseMaleta;
+    }
+
+    // ------------------------------------------------------------------------
+    // ------------------------------- Setters --------------------------------
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPaisSede(String paisSede) {
+        this.paisSede = paisSede;
+    }
+
+    public void setPrecioBaseMaleta(float precioBaseMaleta) {
+        this.precioBaseMaleta = precioBaseMaleta;
     }
 
     public void setPesoBaseMaleta(float pesoBaseMaleta) {
