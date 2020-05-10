b6fff1c5fc1cab3af37a1a12ecda6041983c1f77
Dani <52750032+bydanimos@users.noreply.github.com>
2020-05-10 11:51:30 +0200

    Bonito

--- src/aplicacion/usuarios/PersonalExterno.java
+++ src/aplicacion/usuarios/PersonalExterno.java
@@ -1,26 +1,33 @@
+
 package aplicacion.usuarios;
 
-public class PersonalExterno extends Usuario{
+public final class PersonalExterno extends Usuario{
 
     private boolean estarDentro;
 
+    // -------------------------------------------------------------------------
+    // ------------------------------- Constructor -----------------------------
     public PersonalExterno(String dni, String id, String contrasenha, String correoElectronico, String nombre,
                          String apellido1, String apellido2, TipoSexo sexo, String paisProcedencia, Integer telefono,
-                         boolean estarDentro){
+                         boolean estarDentro) {
         super(dni, id, contrasenha, correoElectronico, nombre, apellido1, apellido2, sexo, paisProcedencia, telefono);
         this.estarDentro = estarDentro;
     }
     
     public PersonalExterno(String dni, String id, String contrasenha, String correoElectronico, String nombre,
-                        String apellido1, String apellido2, TipoSexo sexo, String paisProcedencia, Integer telefono){
+                        String apellido1, String apellido2, TipoSexo sexo, String paisProcedencia, Integer telefono) {
         super(dni, id, contrasenha, correoElectronico, nombre, apellido1, apellido2, sexo, paisProcedencia, telefono);
         this.estarDentro = false;
     }
 
+    // -------------------------------------------------------------------------
+    // ---------------------------------- Getter -------------------------------
     public boolean isEstarDentro() {
         return estarDentro;
     }
 
+    // -------------------------------------------------------------------------
+    // --------------------------------- Setter --------------------------------
     public void setEstarDentro(boolean estarDentro) {
         this.estarDentro = estarDentro;
     }
