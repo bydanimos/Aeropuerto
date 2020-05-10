75bf2d25a7363ae0fed44b827d048adc009b33c6
Dani <52750032+bydanimos@users.noreply.github.com>
2020-05-10 11:40:51 +0200

    Bonito

--- src/aplicacion/vuelos/Maleta.java
+++ src/aplicacion/vuelos/Maleta.java
@@ -1,24 +1,32 @@
+
 package aplicacion.vuelos;
 
 public class Maleta {
+    
     private int id;
     private float peso;
 
+    // -------------------------------------------------------------------------
+    // ------------------------------- Constructor -----------------------------
     public Maleta(int id, float peso) {
         this.id = id;
         this.peso = peso;
     }
 
+    // -------------------------------------------------------------------------
+    // --------------------------------- Getters -------------------------------
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    
     public float getPeso() {
         return peso;
+    }
+    
+    // -------------------------------------------------------------------------
+    // --------------------------------- Setters -------------------------------
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setPeso(float peso) {
