b07687d685ed45f9fd35cbeeb7c1a824ccef237f
Dani <52750032+bydanimos@users.noreply.github.com>
2020-05-10 11:47:18 +0200

    Bonito

--- src/aplicacion/vuelos/Vuelo.java
+++ src/aplicacion/vuelos/Vuelo.java
@@ -1,3 +1,4 @@
+
 package aplicacion.vuelos;
 
 import aplicacion.aviones.Terminal;
@@ -19,7 +20,11 @@ public class Vuelo {
     private int puertaEmbarque;
     private boolean cancelado;
 
-    public Vuelo(Terminal terminal, Avion avion, String numeroVuelo, String destino, String origen, Timestamp fechaSalida, Timestamp fechaSalidaReal, Timestamp fechaLlegada, Timestamp fechaLlegadaReal, float precioActual, int puertaEmbarque, boolean cancelado) {
+    // -------------------------------------------------------------------------
+    // ------------------------------- Constructor -----------------------------
+    public Vuelo(Terminal terminal, Avion avion, String numeroVuelo, String destino, String origen, 
+                 Timestamp fechaSalida, Timestamp fechaSalidaReal, Timestamp fechaLlegada, 
+                 Timestamp fechaLlegadaReal, float precioActual, int puertaEmbarque, boolean cancelado) {
         this.terminal = terminal;
         this.avion = avion;
         this.numeroVuelo = numeroVuelo;
@@ -34,101 +39,103 @@ public class Vuelo {
         this.cancelado = cancelado;
     }
 
+    // -------------------------------------------------------------------------
+    // --------------------------------- Getters -------------------------------
     public Terminal getTerminal() {
         return terminal;
-    }
-
-    public void setTerminal(Terminal terminal) {
-        this.terminal = terminal;
     }
 
     public Avion getAvion() {
         return avion;
     }
-
-    public void setAvion(Avion avion) {
-        this.avion = avion;
-    }
-
+    
     public String getNumeroVuelo() {
         return numeroVuelo;
-    }
-
-    public void setNumeroVuelo(String numeroVuelo) {
-        this.numeroVuelo = numeroVuelo;
     }
 
     public String getDestino() {
         return destino;
     }
 
-    public void setDestino(String destino) {
-        this.destino = destino;
-    }
-
     public String getOrigen() {
         return origen;
-    }
-
-    public void setOrigen(String origen) {
-        this.origen = origen;
     }
 
     public Timestamp getFechaSalida() {
         return fechaSalida;
     }
 
-    public void setFechaSalida(Timestamp fechaSalida) {
-        this.fechaSalida = fechaSalida;
-    }
-
     public Timestamp getFechaSalidaReal() {
         return fechaSalidaReal;
+    }
+    
+    public Timestamp getFechaLlegada() {
+        return fechaLlegada;
+    }
+
+    public Timestamp getFechaLlegadaReal() {
+        return fechaLlegadaReal;
+    }
+    
+    public float getPrecioActual() {
+        return precioActual;
+    }
+    
+    public int getPuertaEmbarque() {
+        return puertaEmbarque;
+    }
+    
+    public boolean isCancelado() {
+        return cancelado;
+    }
+
+    // -------------------------------------------------------------------------
+    // --------------------------------- Setters -------------------------------
+    public void setTerminal(Terminal terminal) {
+        this.terminal = terminal;
+    }
+
+    public void setAvion(Avion avion) {
+        this.avion = avion;
+    }
+
+    public void setNumeroVuelo(String numeroVuelo) {
+        this.numeroVuelo = numeroVuelo;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public void setFechaSalida(Timestamp fechaSalida) {
+        this.fechaSalida = fechaSalida;
     }
 
     public void setFechaSalidaReal(Timestamp fechaSalidaReal) {
         this.fechaSalidaReal = fechaSalidaReal;
     }
 
-    public Timestamp getFechaLlegada() {
-        return fechaLlegada;
-    }
-
     public void setFechaLlegada(Timestamp fechaLlegada) {
         this.fechaLlegada = fechaLlegada;
-    }
-
-    public Timestamp getFechaLlegadaReal() {
-        return fechaLlegadaReal;
     }
 
     public void setFechaLlegadaReal(Timestamp fechaLlegadaReal) {
         this.fechaLlegadaReal = fechaLlegadaReal;
     }
 
-    public float getPrecioActual() {
-        return precioActual;
-    }
-
     public void setPrecioActual(float precioActual) {
         this.precioActual = precioActual;
-    }
-
-    public int getPuertaEmbarque() {
-        return puertaEmbarque;
     }
 
     public void setPuertaEmbarque(int puertaEmbarque) {
         this.puertaEmbarque = puertaEmbarque;
     }
 
-    public boolean isCancelado() {
-        return cancelado;
-    }
-
     public void setCancelado(boolean cancelado) {
         this.cancelado = cancelado;
-    }
-
-    
+    } 
 }
